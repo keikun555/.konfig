129516761873a9211a2288ff947cfab89a3de1ea
Iridium User <ir@irp242-v16g07.dev.purestorage.com>
2020-08-12 17:31:48 +0000

    better smux and added go for ycm

--- ycm_update.sh
+++ ycm_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source globals.sh
-python3 $BUNDLE_DIR/YouCompleteMe/install.py --clang-completer --java-completer --ts-completer
+python3 $BUNDLE_DIR/YouCompleteMe/install.py --clang-completer --java-completer --ts-completer --go-completer
