c4b9b3ea40314f0a199873e960fe41d479f71296
Kei Imada <>
2023-10-06 11:36:38 -0400

    yabai - configure_spaces bug made every space put into the yabai command

--- yabai/.config/yabai/configure_spaces.sh
+++ yabai/.config/yabai/configure_spaces.sh
@@ -2,7 +2,7 @@
 
 NON_MAIN_DISPLAY_SPACES="$(yabai -m query --displays | jq -r '.[] | select(.id == 1 | not) | .spaces | @sh')"
 
-for space in "${NON_MAIN_DISPLAY_SPACES[@]}"
+for space in ${NON_MAIN_DISPLAY_SPACES}
 do
   echo "yabai -m space $space --padding rel:30:0:0:0"
   yabai -m space $space --padding rel:30:0:0:0
