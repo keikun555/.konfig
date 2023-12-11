fedb78aac8b046292ea945a8de8024bc92672434
Kei Imada <>
2023-12-11 08:53:43 -0800

    home directory dynamic fzf scripts

--- fzf_shell/.fzf.bash
+++ fzf_shell/.fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/kei/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/kei/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/kei/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/kei/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
