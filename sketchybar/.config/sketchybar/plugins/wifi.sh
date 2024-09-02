31c33598f7b648cf1fcfe4f92113bdeeef226cf6
Kei Imada <>
2024-09-02 10:24:11 -0400

    sonoma wifi plugin fix

--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++ sketchybar/.config/sketchybar/plugins/wifi.sh
@@ -2,6 +2,10 @@
 
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
+#
+# https://github.com/FelixKratz/SketchyBar/issues/407
+INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
+
 
 if [ "$SENDER" = "wifi_change" ]; then
   WIFI=${INFO:-"Not Connected"}
