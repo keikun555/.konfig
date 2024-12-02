1da8a3a8a0d3aff15b8b7bc061128c3f7d7c6942
Kei Imada <>
2024-12-01 23:00:28 -0500

    layouts for saving layouts and loading them

---
+++ yabai/.config/yabai/layout.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+filename=${1:-desktop}
+directory=$HOME/.config/yabai/layout/$(hostname)
+
+mkdir -p $directory
+
+yabai -m query --windows | jq -re '.[] | select(.minimized != 1) | "yabai -m window \(.id) --display \(.display) --space \(.space) --move abs:\(.frame.x):\(.frame.y) --resize abs:\(.frame.w):\(.frame.h)"' > $directory/$filename.sh
+
+if [[ $? != 0 ]] ; then
+    echo -n "Yabai: failed to save"
+      exit 1
+fi
+
+chmod +x $directory/$filename.sh
+
