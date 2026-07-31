195cfcf8feb7ed22a17e082b31fa5a9b8161dd7a
Kei Imada <>
2026-07-31 11:48:33 -0400

    attempt fixing window management

---
+++ yabai/.config/yabai/yabai.rb
@@ -0,0 +1,48 @@
+class Yabai < Formula
+  desc "A tiling window manager for macOS based on binary space partitioning."
+  homepage "https://github.com/asmvik/yabai"
+  url "https://github.com/asmvik/yabai/releases/download/v7.1.24/yabai-v7.1.24.tar.gz"
+  sha256 "627487c6c390468ea86795198ecf2eab0dd90a6645f4a42225f9720b95b94a43"
+  head "https://github.com/asmvik/yabai.git"
+
+  depends_on :macos => :big_sur
+
+  def install
+    man.mkpath
+
+    if build.head?
+      system "make", "-j1", "install"
+      system "codesign", "-fs", "-", "#{buildpath}/bin/yabai"
+    end
+
+    bin.install "#{buildpath}/bin/yabai"
+    (pkgshare/"examples").install "#{buildpath}/examples/yabairc"
+    (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
+    man1.install "#{buildpath}/doc/yabai.1"
+  end
+
+  def caveats; <<~EOS
+    Copy the example configuration into your home directory:
+      cp #{opt_pkgshare}/examples/yabairc ~/.yabairc
+      cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
+
+    If you want yabai to be managed by launchd (start automatically upon login):
+      yabai --start-service
+
+    When running as a launchd service logs will be found in:
+      /tmp/yabai_<user>.[out|err].log
+
+    If you are using the scripting-addition; remember to update your sudoers file:
+      sudo visudo -f /private/etc/sudoers.d/yabai
+
+    Build the configuration row by running:
+      echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(\which yabai) | cut -d " " -f 1) $(\which yabai) --load-sa"
+
+    README: https://github.com/asmvik/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
+    EOS
+  end
+
+  test do
+    assert_match "yabai-v#{version}", shell_output("#{bin}/yabai --version")
+  end
+end
